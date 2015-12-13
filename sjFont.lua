d3ee80c3d3c8ea25b5dbe5b7f06521919d072f01
Nils Olsson <nilso@enosis.net>
2015-12-13 15:01:41 -0800

    Add more fonts, finish initial functionality

--- sjFont.lua
+++ sjFont.lua
@@ -1,99 +1,191 @@
+
+sjFont = AceLibrary("AceAddon-2.0"):new(
+"AceConsole-2.0",
+"AceDebug-2.0",
+"AceDB-2.0",
+"AceEvent-2.0")
+
 local ADDON_PATH = "Interface\\AddOns\\sjFont\\"
-local SYSTEM_FONTS = {
-    -- Font, Default size
-    { SystemFont,        15 },
+local FONT_PATH = ADDON_PATH.."media\\font\\"
 
-    { GameFontNormal,    12 },
-    { GameFontHighlight, 12 },
-    { GameFontGreen,     12 },
-    { GameFontRed,       12 },
-    { GameFontBlack,     12 },
-    { GameFontWhite,     12 },
-    { GameFontDisable,   12 },
-
-    { ChatFrameEditBoxHeader, 14 },
-    { ChatFrameEditBox, 14 },
-
-    { GameFontNormalSmall, 10 },
-    { GameFontHighlightSmall, 10 },
-    { GameFontDisableSmall, 10 },
-    { GameFontDarkGraySmall, 10 },
-    { GameFontGreenSmall, 10 },
-    { GameFontRedSmall, 10 },
-    { GameFontHighlightSmallOutline, 10 },
-    { GameFontNormalLarge, 16 },
-    { GameFontHighlightLarge, 16 },
-    { GameFontDisableLarge, 16 },
-    { GameFontGreenLarge, 16 },
-    { GameFontRedLarge, 16 },
-
-    { NumberFontNormal, 14 },
-    { NumberFontNormalYellow, 14 },
-    { NumberFontNormalSmall, 12 },
-    { NumberFontNormalSmallGray, 12 },
-    { NumberFontNormalLarge, 16 },
-    { NumberFontNormalHuge, 30 },
-
-    { ChatFontNormal, 14 },
-
-    { QuestTitleFont, 18 },
-    { QuestFont, 13 },
-    { QuestFontNormalSmall, 12 },
-    { QuestFontHighlight, 14 },
-    { ItemTextFontNormal, 15 },
-    { SubSpellFont, 10 },
-    { DialogButtonNormalText, 16 },
-    { DialogButtonHighlightText, 16 },
-
-    { ZoneTextFont, 102 },
-    { SubZoneTextFont, 26 },
-    { ErrorFont, 16 },
-    { TextStatusBarText, 14 },
-    { TextStatusBarTextSmall, 12 },
-    { CombatLogFont, 12 },
-    { GameTooltipText, 12 },
-    { GameTooltipTextSmall, 10 },
-    { GameTooltipHeaderText, 14 },
-
-    { WorldMapTextFont, 102 },
-    { MailTextFontNormal, 15 },
-    { InvoiceTextFontNormal, 12 },
-    { InvoiceTextFontSmall, 10 }
+-- My fonts
+local FONTS = {
+    -- Font name, Default size
+    "Enigmatic",
+    "Gentium",
+    "LiberationSans",
+    "MetalLord",
+    "MyriadCondensed",
+    "VeraSerif"
 }
-local fmt = "|cff22ff55sjFont:|r %s"
+local FONT_USAGE = "{ [1]:Default"
+for i,v in FONTS do
+    FONT_USAGE = FONT_USAGE..format(" | [%s]:%s", i+1, v)
+end
+FONT_USAGE = FONT_USAGE.." }"
 
-function P(msg, ...)
-    DEFAULT_CHAT_FRAME:AddMessage(format(msg, unpack(arg)))
+-- Font objects
+local FRIZQT = "Fonts\\FRIZQT__.TTF"
+local ARIALN = "Fonts\\ARIALN.TTF"
+local MORPHE = "Fonts\\MORPHEUS.TTF"
+local SKURRI = "Fonts\\skurri.ttf"
+local SYSTEM_FONTS = {
+    -- Font object, Default size
+    { SystemFont, FRIZQT, 15 },
+    { GameFontNormal, FRIZQT, 12 },
+    { GameFontHighlight, FRIZQT, 12 },
+    { GameFontGreen, FRIZQT, 12 },
+    { GameFontRed, FRIZQT, 12 },
+    { GameFontBlack, FRIZQT, 12 },
+    { GameFontWhite, FRIZQT, 12 },
+    { GameFontDisable, FRIZQT, 12 },
+    { GameFontNormalSmall, FRIZQT, 10 },
+    { GameFontHighlightSmall, FRIZQT, 10 },
+    { GameFontDisableSmall, FRIZQT, 10 },
+    { GameFontDarkGraySmall, FRIZQT, 10 },
+    { GameFontGreenSmall, FRIZQT, 10 },
+    { GameFontRedSmall, FRIZQT, 10 },
+    { GameFontHighlightSmallOutline, FRIZQT, 10 },
+    { GameFontNormalLarge, FRIZQT, 16 },
+    { GameFontHighlightLarge, FRIZQT, 16 },
+    { GameFontDisableLarge, FRIZQT, 16 },
+    { GameFontGreenLarge, FRIZQT, 16 },
+    { GameFontRedLarge, FRIZQT, 16 },
+    { ChatFrameEditBoxHeader, ARIALN, 14 },
+    { ChatFrameEditBox, ARIALN, 14 },
+    { ChatFontNormal, ARIALN, 14 },
+    { NumberFontNormal, ARIALN, 14, "OUTLINE" },
+    { NumberFontNormalYellow, ARIALN, 14, "OUTLINE" },
+    { NumberFontNormalSmall, ARIALN, 12, "OUTLINE" },
+    { NumberFontNormalSmallGray, ARIALN, 12, "OUTLINE" },
+    { NumberFontNormalLarge, ARIALN, 16, "OUTLINE" },
+    { NumberFontNormalHuge, SKURRI, 30, "OUTLINE" },
+    { QuestTitleFont, MORPHE, 18 },
+    { QuestFont, FRIZQT, 13 },
+    { QuestFontNormalSmall, FRIZQT, 12 },
+    { QuestFontHighlight, FRIZQT, 14 },
+    { ItemTextFontNormal, MORPHE, 15 },
+    { SubSpellFont, FRIZQT, 10 },
+    { DialogButtonNormalText, FRIZQT, 16 },
+    { DialogButtonHighlightText, FRIZQT, 16 },
+    { ZoneTextFont, FRIZQT, 102, "THICKOUTLINE" },
+    { SubZoneTextFont, FRIZQT, 26, "THICKOUTLINE" },
+    { ErrorFont, FRIZQT, 16 },
+    { TextStatusBarText, ARIALN, 14, "OUTLINE" },
+    { TextStatusBarTextSmall, ARIALN, 12, "OUTLINE", "MONOCHROME" },
+    { CombatLogFont, FRIZQT, 12 },
+    { GameTooltipText, FRIZQT, 12 },
+    { GameTooltipTextSmall, FRIZQT, 10 },
+    { GameTooltipHeaderText, FRIZQT, 14 },
+    { WorldMapTextFont, FRIZQT, 102, "THICKOUTLINE" },
+    { MailTextFontNormal, MORPHE, 15 },
+    { InvoiceTextFontNormal, FRIZQT, 12 },
+    { InvoiceTextFontSmall, FRIZQT, 10 }
+}
+
+function sjFont.OnInitialize()
+    sjFont.fonts = FONTS
+    sjFont.font_names = FONT_NAMES
+    sjFont.system_fonts = SYSTEM_FONTS
+
+    -- Chat command
+    sjFont:RegisterChatCommand({ "/sjFont", "/sjf" }, {
+        type = "group",
+        args = {
+            update = {
+                name = "Update Fonts",
+                desc = "Update system fonts",
+                type = "execute",
+                func = sjFont.UpdateSystemFonts
+            },
+            font = {
+                name = "Font",
+                desc = "Set the font",
+                type = "text",
+                get = function()
+                    return sjFont.opt.font
+                end,
+                set = function(set)
+                    if set == "1" then
+                        sjFont.opt.font = "Default"
+                    else
+                        set = tonumber(set)-1
+                        sjFont:Debug("Selected font \"%s%s\"",FONT_PATH,FONTS[set])
+                        sjFont.opt.font = FONTS[set]
+                    end
+                    sjFont.UpdateSystemFonts()
+                end,
+                validate = function(set)
+                    set = tonumber(set)
+                    local valid = set and set >= 1 and set <= getn(FONTS)+1
+                    return valid
+                end,
+                usage = FONT_USAGE
+            },
+            scale = {
+                name = "Font scale",
+                desc = "Set the font scale",
+                type = "range",
+                get = function()
+                    return sjFont.opt.font_scale
+                end,
+                set = function(set)
+                    sjFont.opt.font_scale = set
+                    sjFont.UpdateSystemFonts()
+                end,
+                min = 0,
+                max = 3,
+                step = 0.01,
+                bigStep = 0.1,
+                isPercent = true
+            },
+            debug = {
+                name = "Debug",
+                desc = "Toggle AceDebug",
+                type = "toggle",
+                get = function()
+                    return sjFont.opt.debug
+                end,
+                set = function(set)
+                    sjFont.opt.debug = set
+                    sjFont:SetDebugging(set)
+                end
+            }
+        }
+    })
+
+    -- Saved variables
+    sjFont:RegisterDB("sjFont_DB")
+    sjFont:RegisterDefaults("profile", {
+        debug = true,
+        font = "MyriadCondensed",
+        font_scale = 1
+    })
+    sjFont.opt = sjFont.db.profile
+
+    sjFont:SetDebugging(sjFont.opt.debug)
 end
 
-function sjFont_OnLoad()
-    P(fmt,"sjFont_OnLoad")
-    SlashCmdList["SJFONT"] = sjFont_ChatHandler
-    SLASH_SJFONT1 = "/sjfont"
-    SLASH_SJFONT2 = "/sjf"
+function sjFont.OnEnable()
+    CHAT_FONT_HEIGHTS = {
+        4, 5, 6, 7, 8, 9, 10, 11,
+        12, 13, 14, 15, 16, 17, 18,
+        19, 20, 21, 22, 23, 24, 28, 32
+    }
 
-    sjFont.font = ADDON_PATH.."media\\fonts\\MyriadCondensed.ttf"
-    sjFont.font_scale = 0.5
+    sjFont.UpdateSystemFonts()
 end
 
-function sjFont_OnEvent()
-    P("sjFont_OnEvent")
+function sjFont.OnEvent()
 end
 
-function sjFont_OnUpdate()
-    P("sjFont_OnUpdate")
+function sjFont.OnUpdate()
 end
 
-function sjFont_UpdateSystemFonts()
-    P("sjFont_UpdateSystemFonts")
+function sjFont.UpdateSystemFonts()
     for i,v in SYSTEM_FONTS do
-        local font = v[1]:GetFont()
-        P(fmt,format("%s, %s",font, v[2]*sjFont.font_scale))
-        v[1]:SetFont(font, v[2] * sjFont.font_scale)
+        local font_obj, default_font, size, outline, monochrome = unpack(v)
+        local font = sjFont.opt.font == "Default" and default_font or FONT_PATH..sjFont.opt.font..".ttf"
+        font_obj:SetFont(font, size * sjFont.opt.font_scale, outline, monochrome)
     end
-end
-
-function sjFont_ChatHandler(msg)
-    P(fmt,"sjFont_ChatHandler")
-    sjFont_UpdateSystemFonts()
 end
